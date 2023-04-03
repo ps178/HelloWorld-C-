400f4ef05e2a91361f9f6429fdc4e125accdabb3
Petek Sener <peteksener9@gmail.com>
2023-04-03 17:20:55 +0300

    Initial Commit

---
+++ HelloWorld-C++/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  HelloWorld-C++
+//
+//  Created by Petek Sener on 03/04/2023.
+//g
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
